c42d092acb35e2248a799b7a8b9f1257ecfc6177
NERMEEN NASIM <nermeen.nasim@hotmail.com>
2023-02-27 11:02:38 -0800

    working api for GetAllVillas and GetVilla by Id, with documentation on Swagger UI

---
+++ MagicVilla_VillaAPI/Models/DTOs/VillaDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicVilla_VillaAPI.Models.DTOs
+{
+    public class VillaDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,12 @@
+﻿namespace MagicVilla_VillaAPI.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        //forexample in real db, we have one more field of record creation
+        //we dont want to expose it to api
+
+        public DateTime? CreatedDate { get; set; }
+    }
+}
